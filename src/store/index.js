6b5f4e190ed97e837aad206243ae02c6d7e819a4
yidianshenshen <usr001@greendeMBP.lan>
2020-08-16 21:17:20 +0800

    vuex和router的使用

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,15 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 组件的状态在这里进行一个集中的管理
   state: {
+    count: 0
   },
+  // 可以去改变Vuex里面状态的一个方法集
   mutations: {
+    increase () {
+      this.state.count++
+    }
   },
   actions: {
   },
---
+++ src/views/Info.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        hello info compontent
+        <button type="button" @click="add()">添加</button>
+    </div>
+</template>
+<script>
+import store from '@/store'
+export default {
+    name: 'Info',
+    store,
+    data () {
+        return {
+            msg: 'hello vue'
+        }
+    },
+    mounted () {
+        window.vue = this
+    },
+    methods: {
+        add () {
+            console.log('add Envent from info!')
+            debugger
+            store.commit('increase')
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
